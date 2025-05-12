00a6827b38ac4d338170a74e95f367113d822db2
Alezinho157 <doparaguaileleco@gmail.com>
2025-05-12 11:41:03 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -103,3 +103,26 @@ local humanoid = game.Players.LocalPlayer.Character:WaitForChild("Humanoid")
 _G.SpiderToggle = not _G.SpiderToggle
 humanoid:SetStateEnabled(Enum.HumanoidStateType.Climbing, _G.SpiderToggle)
 end 
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local noclip = _G.NoclipToggle
+
+if noclip then
+    local conn
+    conn = game:GetService("RunService").Stepped:Connect(function()
+        if not _G.NoclipToggle then
+            conn:Disconnect()
+        end
+        for _, part in pairs(char:GetChildren()) do
+            if part:IsA("BasePart") then
+                part.CanCollide = false
+            end
+        end
+    end)
+else
+    for _, part in pairs(char:GetChildren()) do
+        if part:IsA("BasePart") then
+            part.CanCollide = true
+        end
+    end
+end
